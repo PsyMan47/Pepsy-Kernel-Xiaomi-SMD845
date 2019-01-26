10075665ac7de849e97aab0929ec674e2522c6a5
Xhoni Rexhepi <xhonirexhepi@gmail.com>
2019-01-26 17:29:16 +0000

    Add build scripts, AnyKernel2 and Wercker configs

---
+++ beryllium.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+KERNEL_DIR=$PWD
+ANYKERNEL_DIR=$KERNEL_DIR/AnyKernel2
+TOOLCHAINDIR=/pipeline/build/root/toolchain/aarch64-linux-android-4.9
+DATE=$(date +"%d%m%Y")
+KERNEL_NAME="Pepsy"
+DEVICE="-beryllium-"
+VER="-v0.1"
+TYPE="-AOSP"
+FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$TYPE""$VER".zip
+
+rm $ANYKERNEL_DIR/beryllium/Image.gz-dtb
+rm $KERNEL_DIR/arch/arm64/boot/Image.gz $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb
+
+export ARCH=arm64
+export KBUILD_BUILD_USER="Psy_Man"
+export KBUILD_BUILD_HOST="PsyBuntu"
+export CC=/pipeline/build/root/toolchain/proprietary_vendor_qcom_sdclang-6.0_linux-x86_64/bin/clang
+export CXX=/pipeline/build/root/toolchain/proprietary_vendor_qcom_sdclang-6.0_linux-x86_64/bin/clang++
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=$TOOLCHAINDIR/bin/aarch64-linux-android-
+
+make clean && make mrproper
+make beryllium_defconfig
+make -j$( nproc --all )
+
+{
+cp $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb $ANYKERNEL_DIR/beryllium
+} || {
+  if [ $? != 0 ]; then
+    curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Build failed for beryllium :c" -d chat_id=@pepsykernel;
+    exit
+  fi
+}
+
+cd $ANYKERNEL_DIR/beryllium
+zip -r9 $FINAL_ZIP * -x *.zip $FINAL_ZIP
+curl -F chat_id="-1001152658251" -F document=@"$FINAL_ZIP" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
+mv $FINAL_ZIP /pipeline/output/$FINAL_ZIP
+
